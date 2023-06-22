4bc84fd41c6bc0b93f384a5a0e4294e9e731e9de
Pietro Giordano <giopi87@gmail.com>
2023-06-22 17:40:49 +0200

    show / manca stile generale

--- src/main/java/org/lessons/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/org/lessons/springlamiapizzeriacrud/controller/PizzaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,5 +23,22 @@ public class PizzaController {
         List<Pizza> pizza = repository.findAll();
         model.addAttribute("pizzaList", pizza);
         return "/pizza/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(Model model, @PathVariable Integer id) {
+        Pizza showPizza = null;
+        try {
+            List<Pizza> pizza = repository.findAll();
+            for(Pizza p : pizza) {
+                if(p.getId() == id){
+                    showPizza = p;
+                }
+            }
+        }  catch(IndexOutOfBoundsException e) {
+            return "redirect:/pizza";
+        }
+        model.addAttribute("pizza", showPizza);
+        return "/pizza/show";
     }
 }
