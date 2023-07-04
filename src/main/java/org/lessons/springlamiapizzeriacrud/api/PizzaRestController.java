7a95c08c93ac2f1e44c0946cb2b9887095d95b20
Pietro Giordano <giopi87@gmail.com>
2023-07-04 17:25:53 +0200

    aggiunta paginazione ad index

--- src/main/java/org/lessons/springlamiapizzeriacrud/api/PizzaRestController.java
+++ src/main/java/org/lessons/springlamiapizzeriacrud/api/PizzaRestController.java
@@ -4,6 +4,9 @@ import jakarta.validation.Valid;
 import org.lessons.springlamiapizzeriacrud.model.Pizza;
 import org.lessons.springlamiapizzeriacrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,9 +22,17 @@ public class PizzaRestController {
       @Autowired
       private PizzaRepository pizzaRepository;
 
+      /*
       @GetMapping
       public List<Pizza> index() {
             return pizzaRepository.findAll();
+      }
+      */
+
+      @GetMapping
+      public Page<Pizza> page(@RequestParam(defaultValue = "3") Integer size, @RequestParam(defaultValue = "0") Integer page) {
+            Pageable pageable = PageRequest.of(page, size);
+            return pizzaRepository.findAll(pageable);
       }
 
       @GetMapping("{id}")
